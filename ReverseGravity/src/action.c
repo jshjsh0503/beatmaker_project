35e4e4bfe3ddf115040a9a7159904e042d71259d
jamin1052 <jamin1052@naver.com>
2025-12-06 20:02:01 +0900

    aasdfg

--- ReverseGravity/src/action.c
+++ ReverseGravity/src/action.c
@@ -5,6 +5,7 @@
 #include "player_draw.h"
 #include "defs.h"
 #include "action.h"
+#include "map.h"
 
 #define JUMP_SPEED 900.0
 
@@ -12,9 +13,6 @@ void ActGame(void)
 {
     const double dt = 1.0 / 60.0;
 
-    // ---------------------------------------
-    // 중력 반전 (SPACE)
-    // ---------------------------------------
     if (app.key_space && player.is_grounded)
     {
         player.gravity_inverted = !player.gravity_inverted;
@@ -27,46 +25,29 @@ void ActGame(void)
         app.key_space = 0;
     }
 
-    // ---------------------------------------
-    // 가로 이동
-    // ---------------------------------------
     move_player_horzontal(dt);
 
-    // ---------------------------------------
-    // 점프
-    // ---------------------------------------
     if (app.key_down && player.is_grounded)
     {
         double jump_dir = player.gravity_inverted ? 1.0 : -1.0;
         player.v_y = JUMP_SPEED * jump_dir;
         player.is_grounded = 0;
-
         app.key_down = 0;
     }
 
-    // ---------------------------------------
-    // 중력 및 충돌
-    // ---------------------------------------
     gravity_inverted(dt);
     resolve_vertical_collision();
-
     check_spike_collision();
     check_goal_reach();
 
-    // ---------------------------------------------------------
-    // ★★★ 화면 밖 이동 → 방 전환 처리 (여기 추가해야 했음)
-    // ---------------------------------------------------------
-    UpdatePlayer();   // ← 반드시 여기 추가해야 방 이동이 됨!!!
+    // ★ 방 바깥 나갔는지 체크
+    UpdatePlayer();
 
-    // ---------------------------------------------------------
-    // 카메라 이동
-    // ---------------------------------------------------------
-    int room_x = player.pos.x / SCREEN_WIDTH;
-    int room_y = player.pos.y / SCREEN_HEIGHT;
-
-    camera.x = room_x * SCREEN_WIDTH;
-    camera.y = room_y * SCREEN_HEIGHT;
+    // ★ 카메라 끄기(고정)
+    camera.x = 0;
+    camera.y = 0;
 }
+
 
 
 void ActGameOver(void) {
--- ReverseGravity/src/map.h
+++ ReverseGravity/src/map.h
@@ -9,13 +9,9 @@
 extern int current_room_row;
 extern int current_room_col;
 
-// 현재 로드된 방의 맵
 extern int g_map_data[MAP_HEIGHT][MAP_WIDTH];
 
-// 방 로드
 void LoadRoom(int row, int col);
-
-// 방 이동(drow, dcol)
 void ChangeRoom(int drow, int dcol);
 
 #endif
